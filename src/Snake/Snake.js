52e018fabf6a0ea06659e1848e3d2bf3df1787d5
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-30 11:51:09 +0100

    checkIfMovesAreAvailable searching newSnakeHeadPosition by directions

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -48,6 +48,43 @@ class Snake extends React.Component {
 
     gameTick = () => {
         console.log('tick')
+        this.checkIfMovesAreAvailable()
+    }
+
+    checkIfMovesAreAvailable = () => {
+        this.state.snakes.forEach((snakePositions, i) => {
+            const snakeHeadPosition = snakePositions[0]
+            const direction = this.state.directions[i]
+            let newSnakeHeadPosition = null
+
+            switch (direction) {
+                case 'left':
+                    newSnakeHeadPosition = {
+                        x: snakeHeadPosition.x - 1,
+                        y: snakeHeadPosition.y
+                    }
+                    break
+                case 'right':
+                    newSnakeHeadPosition = {
+                        x: snakeHeadPosition.x + 1,
+                        y: snakeHeadPosition.y
+                    }
+                    break
+                case 'top':
+                    newSnakeHeadPosition = {
+                        x: snakeHeadPosition.x,
+                        y: snakeHeadPosition.y - 1
+                    }
+                    break
+                case 'bottom':
+                    newSnakeHeadPosition = {
+                        x: snakeHeadPosition.x,
+                        y: snakeHeadPosition.y + 1
+                    }
+                    break
+                default:
+            }
+        })
     }
 
     composeGameBoard = () => {
