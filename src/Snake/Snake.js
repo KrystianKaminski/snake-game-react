b647beef44c0e35ec2f766f207395f3ae0cb6b05
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-30 09:28:53 +0100

    Snake.js created

---
+++ src/Snake/Snake.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Snake extends React.Component {
+    render() {
+        return (
+            <div>
+                Snake
+            </div>
+        )
+    }
+}
+
+export default Snake
